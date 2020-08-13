1b7fef71215c68257d5f087cbab0c754ca22a1ad
Alzound <61034726+Alzound@users.noreply.github.com>
2020-08-12 22:04:41 -0500

    The other proyect_1
    This is the final version of the game but we need to repair some things

--- Assets/Scripts/Examine.cs
+++ Assets/Scripts/Examine.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class Examine : MonoBehaviour
 {
+
     Camera cam; 
     GameObject selectedObj;
+    RaycastHit hit;
+    public int i = 0; 
 
     Vector3 originPosition;
     Vector3 originRotation;
 
-    bool examinable; 
+    bool examinable;
+    bool destruction = false;
+  
+    RaycastHit Hit;
+
+    public ScrVoiceOverLogic scrVoiceOverLogic;
 
 
     // Start is called before the first frame update
@@ -24,36 +33,87 @@ public class Examine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        onSelectedObj();
+            onSelectedObj();
 
-        turnAround();
+            turnAround();
 
-        ExitExamination();
-
+            ExitExamination();
 
     }
 
+    //All of the following code helps me to know when the raycast hits and object with a the specific tag. 
     private void onSelectedObj()
     {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if(Physics.Raycast(ray, out hit))
+        if (Input.GetMouseButtonDown(0) && examinable == false)
         {
-            selectedObj = hit.transform.gameObject;
 
-            originPosition = selectedObj.transform.position;
-            originRotation = selectedObj.transform.rotation.eulerAngles;
+            //If it is only an item then the player can grab it and then rotate it.
+            //Then the object returns to it's original position because the value of it was save in another variable. 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            selectedObj.transform.position = cam.transform.position + (transform.forward * 3f);
+            if (Physics.Raycast(ray, out hit, 8) && hit.transform.tag == "item")
+            {
 
-            Time.timeScale = 0;
+                selectedObj = hit.transform.gameObject;
 
-            examinable = true; 
+                originPosition = selectedObj.transform.position;
+                originRotation = selectedObj.transform.rotation.eulerAngles;
+
+                selectedObj.transform.position = cam.transform.position + (transform.forward * 3.1f);
+
+                
+                Time.timeScale = 0;
+
+                examinable = true;
+            }
+
+            //Same as before but when it's a keyobject the player takes it, and the game destroys it, so we can save the hard work of doing an inventory. 
+            else if(Physics.Raycast(ray, out hit, 8) && hit.transform.tag == "KeyObject")
+            {
+                scrVoiceOverLogic.ReproduceVoiceOver(hit.transform.gameObject.GetComponent<VoiceOverTrigger>().voiceOverIndex);
+
+                selectedObj = hit.transform.gameObject;
+
+                    originPosition = selectedObj.transform.position;
+                    originRotation = selectedObj.transform.rotation.eulerAngles;
+
+                    selectedObj.transform.position = cam.transform.position + (transform.forward * 3.1f);
+
+
+                Time.timeScale = 0; 
+
+                    destruction = true; 
+                
+            }
+            //Originally this following lines of code where in case the player grabbed the gun in the final segment. 
+            //But the professor didn't like it, so... There's that, a thing that could have been but not very subtle. 
+            /*
+            else if (Physics.Raycast(ray, out hit, 8) && hit.transform.tag == "TheEnd")
+            {
+                Debug.Log("Juasjuas");
+              
+
+
+                selectedObj = hit.transform.gameObject;
+
+                originPosition = selectedObj.transform.position;
+                originRotation = selectedObj.transform.rotation.eulerAngles;
+
+                selectedObj.transform.position = cam.transform.position + (transform.forward * 3.1f);
+
+
+                Time.timeScale = 0;
+
+                theEnd = true; 
+            }
+
+            */
         }
     }
     private void turnAround()
     {
+        //This is going to rotate the object taking the movement from the camera. 
+        //Also, bless Youtube.
         if(Input.GetMouseButton (0) && examinable)
         {
             float rotationsSpeed = 15;
@@ -68,15 +128,27 @@ public class Examine : MonoBehaviour
 
     private void ExitExamination()
     {
-        if(Input.GetMouseButtonDown(1) && examinable)
+        //With this when the player clicks the right button of the mouse the object returns to it's original position.
+        if(Input.GetMouseButtonDown(1) && examinable )
         {
             selectedObj.transform.position = originPosition;
-            selectedObj.transform.position = originRotation;
+            selectedObj.transform.eulerAngles = originRotation;
 
             Time.timeScale = 1;
+            examinable = false;
 
-            examinable = false; 
+           
         }
+        //But if it's this case, and the destruction value coming from keyobject is true then the object is destroyed when pressed right click. 
+        else if (Input.GetMouseButtonDown(1)&&destruction==true)
+        {
+            i++;
+            examinable = false;
+            Destroy(selectedObj);
+            Time.timeScale = 1; 
+         
+        }
+   
     }
 }
 
---
+++ Assets/Scripts/Opendoors.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio; 
+using UnityEngine;
+
+public class Opendoors : MonoBehaviour
+{
+   
+    AudioSource audioData;
+    public int flag;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //A nice sound of opening door.
+        Debug.Log("s");
+        audioData = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        flag = GameObject.Find("Main Camera").GetComponent<Examine>().i;
+        Options();
+        TheEnd();
+    }
+
+    void Options()
+    {
+        switch (flag)
+        {
+            //Well, originally it was an if else statement right here, but then i questioned myself about my life, and one thing led to another, either way, i changed it
+            //the same way she changed me xD (bad pun) so now it takes flag, witch has the value taken from examine whenever an object is pick wich increments i. 
+            //And if you pick one object that door with the corresponding value of i is going to be open... ah... im tired of this.
+            case 1:
+                if (gameObject.tag == "Door1")
+                {
+                    gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+                }
+                break;
+
+            case 2:
+
+                if(gameObject.tag == "Door2")
+                { 
+        
+                    gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
+                }
+
+                break;
+
+            case 3:
+
+                if (gameObject.tag == "Door3")
+                {
+
+                    gameObject.transform.rotation = Quaternion.Euler(0, 1, 0);
+                 
+                }
+
+                break;
+
+            case 4:
+
+                if (gameObject.tag == "Door4")
+                {
+
+                    gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
+
+                }
+
+                break;
+
+            case 5:
+
+                if (gameObject.tag == "Door5")
+                {
+
+                    gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+                }
+
+                break;
+        }       
+    }
+
+    void TheEnd()
+    {
+        if(GameObject.Find("Player").GetComponent<TheEndTrigger>().close==true && gameObject.tag=="Door3")
+        {
+            //When the player enters the collider in the script TheEndTrigger i can enter this void and close the door with the tag Door 3 so the player is trapped, like me... right now. 
+               gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
+
+        }
+    }
+
+}
